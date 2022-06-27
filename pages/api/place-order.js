b8daa9246686f687387492aad9b0d929b3ca8db4
Junaid <junaidkabani3@gmail.com>
2022-06-27 14:44:13 -0500

    Add cors

--- pages/api/place-order.js
+++ pages/api/place-order.js
@@ -1,6 +1,28 @@
 import { createTransport, getTestMessageUrl } from "nodemailer";
+import Cors from "cors";
+
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET", "POST", "OPTIONS"],
+    origin: [],
+  })
+);
 
 export default async (req, res) => {
+  await cors(req, res);
+
   const {
     accessToken,
     email,
